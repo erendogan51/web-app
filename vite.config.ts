08316403a71c521904f5f94f966cbd6d34c7344c
Eren Dogan <eren51@live.at>
2023-10-29 18:20:24 +0100

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,10 @@ import {defineConfig} from 'vite'
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-    // Base URL for your application (e.g., '/my-app/' or an absolute URL like 'https://example.com/')
     base: '',
 
-    // Root directory for your project
     root: './',
 
-    // Build output directory
     build: {
         rollupOptions: {
             input: {
@@ -19,18 +16,12 @@ export default defineConfig({
         },
     },
 
-    // Public directory (files in this directory are copied to the build output)
     publicDir: 'public',
 
-    // Include additional files in the build (e.g., icons, fonts, etc.)
     assetsInclude: ['assets'],
 
-    // Define your Vite plugins here
-    plugins: [],
-
-    // Configure the development server
     server: {
-        port: 3000, // The port to run the development server
-        open: true, // Automatically open the app in your default browser
+        port: 3000,
+        open: true,
     },
 });
